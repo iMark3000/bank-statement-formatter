ea477c6caddd010bb2944de082182da535c83b2a
Mark Young <markyoung.ca@gmail.com>
2024-01-06 10:23:00 -0800

    Added initial setup for main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+def get_statements():
+    pass
+
+
+def move_to_archive():
+    pass
+
+
+def process_wells_fargo():
+    pass
+
+
+def process_capital_one():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
